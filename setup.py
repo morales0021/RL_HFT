be80e72f4b29c65a7951247a5e2d488238969191
Jose <morales0021@gmail.com>
2025-02-17 23:03:30 +0100

    Another update

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='hftrl',
+    version='0.0.1',
+    packages=[
+        'hftrl'
+    ],
+    install_requires=[
+        'pydantic',
+        'gymnasium'
+    ],
+)
+
+
